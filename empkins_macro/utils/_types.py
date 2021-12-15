c92e36ba0caa98a7ea68eb9fc0ef9034e2c1d354
Robert Richer <robert.richer@fau.de>
2021-12-15 15:16:20 +0100

    misc

--- empkins_macro/utils/_types.py
+++ empkins_macro/utils/_types.py
@@ -1,7 +1,7 @@
 """Some custom helper types to make type hints and type checking easier."""
 
 from pathlib import Path
-from typing import Hashable, TypeVar, Union
+from typing import Hashable, Sequence, TypeVar, Union
 
 import numpy as np
 import pandas as pd
@@ -10,6 +10,7 @@ _Hashable = Union[Hashable, str]
 
 path_t = TypeVar("path_t", str, Path)  # pylint:disable=invalid-name
 arr_t = TypeVar("arr_t", pd.DataFrame, pd.Series, np.ndarray)  # pylint:disable=invalid-name
+str_t = TypeVar("str_t", str, Sequence[str])  # pylint:disable=invalid-name
 T = TypeVar("T")
 
 
