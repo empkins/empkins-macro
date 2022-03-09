1ac173a0365881756ef95aa8cb461af067fb9794
Luca Abel <luca.abel@fau.de>
2022-03-09 10:59:59 +0100

    prepared torso bounce

--- empkins_macro/feature_extraction/body_posture_gait/_bouncy.py
+++ empkins_macro/feature_extraction/body_posture_gait/_bouncy.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 
+def torso_bounce(data: pd.DataFrame) -> pd.DataFrame:
+    return _torso_bounce(data)
+
+
 def _torso_bounce(data: pd.DataFrame) -> pd.DataFrame:
     # as defined by 10.1016/j.humov.2017.11.008: mean vertical velocity of chest
 
