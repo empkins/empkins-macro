eb7f96c02017ad57bdcb041a994cf43424dbc184
Robert Richer <robert.richer@fau.de>
2025-05-19 11:26:03 +0200

    after tooling upgrade

--- src/empkins_macro/feature_extraction/__init__.py
+++ src/empkins_macro/feature_extraction/__init__.py
@@ -10,12 +10,12 @@ from empkins_macro.feature_extraction.feature_extraction import (
 )
 
 __all__ = [
-    "relative_to_baseline",
-    "condition_difference",
-    "extract_generic_features",
-    "extract_expert_features",
     "clean_features",
+    "condition_difference",
+    "extract_expert_features",
+    "extract_generic_features",
     "extract_spatio_temporal_features",
-    "extract_tug_features",
     "extract_temporal_features",
+    "extract_tug_features",
+    "relative_to_baseline",
 ]
